98fcfc98600a4f618cdfa31daf684d13d77a9144
Yining <yining@yiningzhao.info>
2012-03-14 00:56:34 -0400

    regular update

--- update-hg-repos.sh
+++ update-hg-repos.sh
@@ -3,6 +3,12 @@ cd ~/vimwiki
 git add .
 git commit -m "regular update"
 git push
+
+cd ~/.vim
+git add .
+git commit -m "regular update"
+git push
+
 cd ~/.dotfiles
 git add .
 git commit -m "regular update"
